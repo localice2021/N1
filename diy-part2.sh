7e4593a8c062f3ec8a5a572d1a18bce016ba577b
localice2021 <94673672+localice2021@users.noreply.github.com>
2023-10-29 00:56:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,7 +19,7 @@
 # echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.13.1/g' package/base-files/files/bin/config_generate
 sed -i "s/'OpenWrt'/'wr703'/g" package/base-files/files/bin/config_generate
 
 #Modify ssid

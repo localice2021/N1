6536f2ab126f1d077e7a19889bd084ada70c37fb
localice2021 <94673672+localice2021@users.noreply.github.com>
2023-11-28 09:21:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+rm -rf package/feeds/luci-app-dockerman
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman

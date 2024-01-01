16c8a72cfb117fa2628a1ff3462f488852ab3acd
localice2021 <94673672+localice2021@users.noreply.github.com>
2024-01-01 21:31:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -52,6 +52,7 @@ sed -i 's/OpenWrt/Phicomm/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 git clone https://github.com/kenzok8/small package/small
 git clone https://github.com/kenzok8/openwrt-packages package/open-packages
+git clone https://github.com/lxiaya/openwrt-homeproxy.git package/luci-app-homeproxy
 # git clone https://github.com/libremesh/lime-packages.git package/lime-packages
 # Add third-party software packages (Specify the package)
 # svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
